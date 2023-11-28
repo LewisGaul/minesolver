1427609ad4cb0c95a38cafc2d18923642763fa1b
Lewis Gaul <lewis.gaul@gmail.com>
2023-11-28 00:43:24 +0000

    Update to zig v0.11

--- zig/README.md
+++ zig/README.md
@@ -2,7 +2,7 @@
 
 Zig code that may be used for solving minesweeper boards (possibly in combination with the rest of this package!).
 
-Uses Zig 0.9.
+Uses Zig 0.11.
 
 
 ## How to Use
--- zig/build.zig
+++ zig/build.zig
@@ -9,26 +9,40 @@ pub fn build(b: *Builder) void {
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
     // Building the main exe.
-    const exe = b.addExecutable("zig-main", "main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.addPackagePath("clap", "deps/zig-clap/clap.zig");
-    exe.install();
-
-    // Running tests.
-    var tests = b.addTest("main.zig");
-
-    // Define the 'test' subcommand.
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&tests.step);
-
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(&exe.step); // TODO: Is this needed?
+    const exe = b.addExecutable(.{
+        .name = "zig-main",
+        .root_source_file = .{ .path = "main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const clap_dep = b.addModule(
+        "clap",
+        .{ .source_file = .{ .path = "deps/zig-clap/clap.zig" } },
+    );
+    exe.addModule("clap", clap_dep);
+    b.installArtifact(exe);
 
     // Define the 'run' subcommand.
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
     const run_step = b.step("run", "Run the script");
     run_step.dependOn(&run_cmd.step);
+
+    // Create a UT step - builds the test executable but does not run it.
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_tests = b.addRunArtifact(tests);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_tests.step);
 }
